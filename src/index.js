dcda8a9e12d8c5e8a3cbd27be5b6fa413722efca
rodrigo-seabra <rodrigo.seabra08>
2024-10-31 19:13:35 -0300

    fix: cors origin for localhost

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL, 
+    origin: process.env.FRONTEND_URL || "http://localhost:3000", 
     methods: 'GET, POST, PUT, DELETE',
     credentials: true, 
 }));
